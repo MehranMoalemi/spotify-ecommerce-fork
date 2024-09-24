b8d39ba3b9c3625060ca0daa83a71e51608a3ac1
mehran <mohammadmoallemi78@gmail.com>
2024-09-24 20:50:10 +0330

    feat(home/ui): add ServiceCard component and types

---
+++ src/pages/home/ui/ServiceCard/ServiceCard.d.tsx
@@ -0,0 +1,4 @@
+interface ServiceCardPropsTypes { 
+    children: React.ReactNode[];
+    title: string;
+}
---
+++ src/pages/home/ui/ServiceCard/index.tsx
@@ -0,0 +1,17 @@
+'use client'
+import React, { useEffect } from 'react'
+
+const ServiceCard: React.FC<ServiceCardPropsTypes> = (props) => {
+  const { children,title } = props;
+  return (
+    <div className='flex flex-col items-center justify-center gap-4 [&>p]:text-center text-black [&>p]:text-md [&>h3]:text-xl [&>h3]:font-semibold w-60 h-52  bg-khaki rounded-3xl px-9 pb-6'>
+      {children[0]}
+      <h3>
+      {title}
+      </h3>
+      {children[1]}
+    </div>
+  )
+}
+
+export default ServiceCard
