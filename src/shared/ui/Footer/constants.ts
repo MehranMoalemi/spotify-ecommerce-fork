2df038c2fd75f182fdb963b75fa94d1771f6fd78
mehran <mohammadmoallemi78@gmail.com>
2024-10-15 14:48:39 +0330

    feat(shared/ui): add footer constants

---
+++ src/shared/ui/Footer/constants.ts
@@ -0,0 +1,13 @@
+export const accessLinks = [
+    { title: "اطلاعات بیشتر در مورد اسپاتیفای", address: "https://www.spotify.com" },
+    { title: "ویژگی‌های اسپاتیفای", address: "https://www.spotify.com/features" },
+    { title: "نحوه ثبت‌نام در اسپاتیفای", address: "https://www.spotify.com/signup" },
+    { title: "صفحه پرمیوم اسپاتیفای", address: "https://www.spotify.com/premium" }
+];
+
+export const relatedLinks = [
+    { title: "صفحه پشتیبانی", address: "https://support.spotify.com" },
+    { title: "سوالات متداول", address: "https://support.spotify.com/faq" },
+    { title: "ارتباط با پشتیبانی", address: "https://support.spotify.com/contact" },
+    { title: "پشتیبانی پرمیوم", address: "https://support.spotify.com/premium" }
+];
