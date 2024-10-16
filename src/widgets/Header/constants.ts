748ed466147a3f0480610dc6e1bfa686303adc6c
mehran <mohammadmoallemi78@gmail.com>
2024-10-16 15:11:54 +0330

    feat(widgets/header): add navigation items constants

---
+++ src/widgets/Header/constants.ts
@@ -0,0 +1,10 @@
+export const navigationItems = [
+  { label: "صفحه اصلی", link: "/" },
+  { label: "تماس با ما", link: "/contactus" },
+  { label: "خرید اشتراک", link: "/packages" },
+];
+
+
+export const lightValues= {
+  home:1,contactus:2,packages:3
+} 
