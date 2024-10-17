669d37cf2b9ce40388d88383db79dd35c334924a
mehran <mohammadmoallemi78@gmail.com>
2024-10-17 08:49:33 +0330

    refactor(shared/ui): add displayName to withSVGWrapper HOC

--- src/shared/ui/hocs/WithSvgWrapper/index.tsx
+++ src/shared/ui/hocs/WithSvgWrapper/index.tsx
@@ -8,10 +8,14 @@ interface WithSVGWrapperProps extends SVGAttributes<SVGElement> {
 export function withSVGWrapper<P extends object>(
   WrappedComponent: ComponentType<P>
 ): React.FC<P & WithSVGWrapperProps> {
-  return ({ additionalStyles, additionalAttributes, ...props }: WithSVGWrapperProps) => {
+  const WithSVGWrapper: React.FC<P & WithSVGWrapperProps> = ({ additionalStyles, additionalAttributes, ...props }) => {
     const combinedStyles = { ...additionalStyles, ...props.style };
     const combinedAttributes = { ...additionalAttributes, ...props };
 
     return <WrappedComponent {...(combinedAttributes as P)} style={combinedStyles} />;
   };
+
+  WithSVGWrapper.displayName = `withSVGWrapper(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
+
+  return WithSVGWrapper;
 }
