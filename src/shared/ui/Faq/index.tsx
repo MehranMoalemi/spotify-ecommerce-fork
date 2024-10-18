e0985ed190cc7e884455a9c89e207191c6bbf170
mehran <mohammadmoallemi78@gmail.com>
2024-10-18 09:53:44 +0330

    styles:fix accordions padding

--- src/shared/ui/Faq/index.tsx
+++ src/shared/ui/Faq/index.tsx
@@ -9,11 +9,11 @@ import {
 
 export const Faq: React.FC<FaqProps> = ({ items, type = "single" }) => {
     return (
-        <Accordion type={type} collapsible className="w-full p-4  mt-10 relative z-[5]">
+        <Accordion type={type} collapsible className="w-full p-4  mt-10 relative z-[5] ">
             <div className="flex flex-col w-full md:flex-row md:flex-wrap gap-4 items-start justify-center">
                 {items.map((item) => (
                     <AccordionItem key={item.value} value={item.value} className="border border-[#FFCDB6] rounded-lg w-full md:w-[45%]">
-                        <AccordionTrigger className="py-2 px-4 rounded-lg text-[#FFCDB6]">
+                        <AccordionTrigger className="py-4 px-4 rounded-lg text-[#FFCDB6]">
                             {item.title}
                         </AccordionTrigger>
                         <AccordionContent className="p-4">
